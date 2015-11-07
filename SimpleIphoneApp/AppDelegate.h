205f4373c77c4107164881c5eba04f6ae7ddc0ed
Kerry Toonen <kerrytoonen@neva-bassingthwaites-macbook-pro.local>
2015-11-07 17:17:35 +0900

    Initial Commit

---
+++ SimpleIphoneApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleIphoneApp
+//
+//  Created by Kerry Toonen on 2015-11-07.
+//  Copyright © 2015 Kerrto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
